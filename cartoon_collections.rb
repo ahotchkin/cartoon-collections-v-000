c00eaafcb1a16e518ce483413db340cb16be57f7
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-20 00:42:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,22 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index { |name, index| puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  new_array = []
+  array.map { |i| new_array << "#{i.capitalize}!"}
+  new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  if array.each { |i| i.length > 4}
+    true
+  elsif array.each { |i| i.length <= 4}
+    false
+  end
 end
 
 def find_the_cheese# code an argument here
