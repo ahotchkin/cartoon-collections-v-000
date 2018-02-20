c95ecd10649d7b4d5e765b89b983ba21e325514c
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-20 00:58:28 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,12 @@ def long_planeteer_calls(array)# code an argument here
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if array.detect { |i| i = cheese_type[i]}
+    true
+  else
+    nil
+  end
 end
