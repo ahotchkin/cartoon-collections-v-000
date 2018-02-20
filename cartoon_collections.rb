60c3925052cbc4a3e04cb7e0c47b86fa61f08e6e
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-20 00:58:36 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -22,7 +22,7 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if array.detect { |i| i = cheese_type[i]}
+  if array.detect { |i| i = cheese_types[i]}
     true
   else
     nil
