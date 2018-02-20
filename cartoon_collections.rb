efd40ac1c1bd3bff71fabdbe1d0ab2477c8da265
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-20 01:15:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,11 +12,7 @@ end
 
 def long_planeteer_calls(array)# code an argument here
   # Your code here
-  if array.each { |i| i.length > 4}
-    true
-  elsif array.each { |i| i.length <= 4}
-    false
-  end
+  array.any? { |i| i.length > 4} ? true : false
 end
 
 def find_the_cheese(array)# code an argument here
